e9b184ae538c5bfef6513d0f2cdce2cf67e6ee8f
Ahmerkhan12 <137258203+Ahmerkhan12@users.noreply.github.com>
2025-09-09 21:39:14 +0500

    Update teacher-chat-interface.tsx

--- components/teacher-chat-interface.tsx
+++ components/teacher-chat-interface.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { useRef, useEffect as useReactEffect } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
